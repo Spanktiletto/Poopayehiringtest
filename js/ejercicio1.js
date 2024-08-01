0dd8a8d74bf3c229b5560155b96794ebdc81af69
Spanktiletto <themrp@hotmail.com>
2024-07-31 19:52:47 -0500

    Ejercicios

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,9 +1,40 @@
 // NO CAMBIES EL NOMBRE DE ESTA FUNCIÓN 
 // Comenta la linea 5 y descomenta la linea 4 para utilziar nodemon o node en este archivo
 // Regresa todo a como estaba para probar los tests
-// function wordCounter (phrase) {
+//function wordCounter (phrase) {
 export function wordCounter (phrase) {
+    let palabras = phrase.split(" ")
+    console.log(palabras)
+    let contador={}
+
+    for (let p of palabras){
+        if(contador[p] !== undefined){
+            contador[p]++
+        }else{
+            contador[p]=1
+        }
+    }       
+    console.log(contador)
+    let a=[]
+    for (let palabra in contador) {
+        let objeto={
+            
+            word: palabra,
+            count: contador[palabra]
+            
+        }
+        a.push(objeto)
+    }
+    
+    let mostrar = a.sort((x,y)=> y.count - x.count)
+  
+   
+    console.log((mostrar[0]))
+
+    return mostrar[0]
+  
 }
 
+
 // Puedes hacer pruebas corriendo node o nodemon y el nombre de tu archivo descomentando esta linea
-wordCounter('Juan es un desarrollador en Google, y como desarrollador trabaja en el área de tecnología y en el área de innovación.')
+wordCounter("Juan es un desarrollador en Google, y como desarrollador trabaja en el área de tecnología y en el área de innovación.")
--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -3,18 +3,39 @@
 import axios from 'axios'
 import fetch from 'node-fetch';
 
-// const axios = require('axios') 
-// const fetch =  require('node-fetch') ;
+//const axios = require('axios') 
+//const fetch =  require('node-fetch') ;
+
 
 // APIs
-// const POKEAPI = 'https://pokeapi.co/api/v2/pokemon/'
-// const URLImages = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/25.png'
+const POKEAPI = 'https://pokeapi.co/api/v2/pokemon/'
+const URLImages = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/25.png'
 
 // NO CAMBIES EL NOMBRE DE ESTA FUNCIÓN
 // Comenta la linea 16 y descomenta la linea 15 para utilziar nodemon en este archivo y regresa ambas como estaban para correr los tests
-// async function getPokemonList (){
-  export async function getPokemonList (){
+//async function getPokemonList (){
+export async function getPokemonList (){
+    try {
+      const newURLImages=URLImages.replace(/25.png/i,"")
+      let response = await fetch(POKEAPI);
+      let data = await response.json();
+      let pokemonList = data.results.map((pokemon, index) => {
+          let id = index + 1;
+          return {
+              name: pokemon.name,
+              image: `${newURLImages}${id}.png`
+          };
+      });
 
+      return pokemonList;
+  } catch (error) {
+      console.error('Error al obtener la lista de pokémones:', error);
+  }
 }
+
+
+getPokemonList()
+  .then(pokemonList => 
+      console.log(pokemonList));
 
 // Para hacer debug puedes colocar console.log() donde lo necesites y correr el test de este ejercicio
